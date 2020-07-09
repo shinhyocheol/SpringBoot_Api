62634e8350572b33cd20bae6e278a9b3c2053453
shinhyocheol <zzzskssk1@naver.com>
2020-07-09 20:57:32 +0900

    2020-07-09  - 잘못된 import 내용 수정

--- src/main/java/kr/co/platform/api/user/dao/impl/ApiUserDAOImpl.java
+++ src/main/java/kr/co/platform/api/user/dao/impl/ApiUserDAOImpl.java
@@ -2,11 +2,9 @@ package kr.co.platform.api.user.dao.impl;
 
 import java.util.Map;
 
-
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import kr.co.mobile_platform.cms.user.dao.CMSUserDAO;
 import kr.co.platform.api.user.dao.ApiUserDAO;
 import kr.co.platform.util.base.BaseDAOImpl;
 
@@ -18,7 +16,7 @@ public class ApiUserDAOImpl extends BaseDAOImpl implements ApiUserDAO {
 	
     @Override
     public int insertUserInfo(Map<String, Object> dataMap) {
-        return 0;
+        return insertUserInfo(dataMap);
     }
 
 }
