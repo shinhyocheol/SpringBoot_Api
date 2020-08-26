d21b7d851010d3da85db253ba74d74ac242438c8
shinhyocheol <shinhyocheol@ginhub.com>
2020-08-26 10:09:31 +0900

    2020-08-26(수)  1. 지뢰찾기 리팩토링

--- src/main/java/kr/co/platform/code/LandMine.java
+++ src/main/java/kr/co/platform/code/LandMine.java
@@ -11,7 +11,7 @@ public class LandMine {
 	private static final String MINE = " * "; 
     
     // 설치 할 지뢰 수
-    private static final int MINE_CNT = 10; 
+    private static final int DEFAULT_MINE_CNT = 10; 
     
     // 로우 수 설정(10)
     private static final int ROW = 10; 
@@ -19,7 +19,7 @@ public class LandMine {
     private static final int COL = 10; 
 
     // 지뢰의 수 혹은 지뢰를 담은 배열(10X10)
-    private static String[][] mineAndCountList; 
+    private String[][] mineAndCountList; 
     
     /**
      * 리스트 초기화
@@ -37,8 +37,10 @@ public class LandMine {
     	
     	LandMine landMine = new LandMine();
     	
-    	landMine.setMineLocation(); // 지뢰 배치
-    	landMine.setBoardValues(); // 자신을 제외하고 주변 8칸의 지뢰 카운트 수집
+    	/** 지뢰 배치 */
+    	landMine.setMine(); 
+    	/** 자신의 위치가 지뢰가 아닌경우 자신을 제외한 주변 8칸의 지뢰 수를 배치 */
+    	landMine.setNearMineCount(); 
     	
     	/** 출력 */
     	System.out.println("ROW : " + ROW);
@@ -48,34 +50,31 @@ public class LandMine {
         System.out.println("--------------------------------");
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COL; j++) {
-                System.out.print(mineAndCountList[i][j]);
+                System.out.print(landMine.mineAndCountList[i][j]);
             }
             System.out.println();
         }
         System.out.println("--------------------------------");
     }
 
-    /**
-     * 지뢰 배치(배치 위치는 랜덤)
-     */
-    public void setMineLocation() {
+    /** 지뢰 배치(배치 위치는 랜덤) */
+    public void setMine() {
     	
     	Random random = new Random();
     	
-        for (int i = 0; i < MINE_CNT; i++) {
+        for (int i = 0; i < DEFAULT_MINE_CNT; i++) {
         	int ranRow = random.nextInt(ROW);
             int ranCol = random.nextInt(COL);
-
+            
+            /** 배치할 위치가 중복될 수 있으므로 중복체크 */
             if (!isMine(ranRow, ranCol)) {
             	mineAndCountList[ranRow][ranCol] = MINE;
             }
 		}
     }
 
-    /**
-     * 배열 내 기준 위치순번에 지뢰가 아닌 경우 주변 지역 지뢰 카운트 배치
-     */
-    public void setBoardValues() {
+    /** 배열 내 기준 위치순번에 지뢰가 아닌 경우 주변 지역 지뢰 카운트 배치 */
+    public void setNearMineCount() {
         for (int i = 0; i < ROW; i++) {
             for (int j = 0; j < COL; j++) {
                 if (isNotMine(i, j)) mineAndCountList[i][j] = nearMineCheck(i, j);
@@ -83,26 +82,26 @@ public class LandMine {
         }
     }
 
-    /**
-     * 기준 위치 주변에 지뢰수 
-     */
+    /** 기준 위치(자신)를 제외한 주변 8개의 지뢰 존재 체크 및 지뢰 수 집계 */
     public String nearMineCheck(int row, int col) {
-        int count = 0;
+        int mineCnt = 0;
+        
+        if (isMine(row - 1, col - 1)) mineCnt++;
+        if (isMine(row - 1, col)) mineCnt++;
+        if (isMine(row - 1, col + 1)) mineCnt++;
+        if (isMine(row, col - 1)) mineCnt++;
+        if (isMine(row, col + 1)) mineCnt++;
+        if (isMine(row + 1, col - 1)) mineCnt++;
+        if (isMine(row + 1, col)) mineCnt++;
+        if (isMine(row + 1, col + 1)) mineCnt++;
 
-        if (isMine(row - 1, col - 1)) count++;
-        if (isMine(row - 1, col)) count++;
-        if (isMine(row - 1, col + 1)) count++;
-        if (isMine(row, col - 1)) count++;
-        if (isMine(row, col + 1)) count++;
-        if (isMine(row + 1, col - 1)) count++;
-        if (isMine(row + 1, col)) count++;
-        if (isMine(row + 1, col + 1)) count++;
-
-        return " " + count + " ";
+        return " " + mineCnt + " ";
     }
 
     /**
      * 기준 위치가 지뢰인 경우 true 리턴
+     * 이전 순번과 다음 순번이 없는 위치는 지뢰체크가 불가능하며 오류가 발생함. 
+     * 따라서 try~catch를 통해  false를 리턴하고 지뢰체크에서 제외 
      */
     private boolean isMine(int row, int col) {
         try {
@@ -111,9 +110,7 @@ public class LandMine {
             return false;
         }
     }
-    /**
-     * 기준 위치가 지뢰가 아닌경우 false 리턴
-     */
+    /** 기준 위치가 지뢰가 아닌경우 false 리턴 */
     private boolean isNotMine(int i, int j) {
         return !isMine(i, j);
     }
